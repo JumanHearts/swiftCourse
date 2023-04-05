abaf8dc14ba9f8a9099a0a3afe82b57bc42d5e28
JumanHearts <129673179+JumanHearts@users.noreply.github.com>
2023-04-06 00:09:21 +0300

    Add files via upload

---
+++ Introduction/Introduction.swift
@@ -0,0 +1,17 @@
+/*:
+ ## Exercise - Use Playgrounds
+*/
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+print("-My Favorite Song is Shadow By Seventeen")
+
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+print("-Some Lyrics of The Song:")
+print("Shadow, my shadow, my shadow")
+print("I don't want others to see you")
+print("Shadow, my shadow, my shadow")
+print("I hated you and hurt you")
+print("Shadow, my shadow, my shadow")
